cb19dc8376a8efd8b2b166e70b2cc615a924fa8e
Rohan Sawant <rohansawantct83@gmail.com>
2020-04-12 18:06:32 +0530

    Updated Room

--- static/ui/src/Room.js
+++ static/ui/src/Room.js
@@ -9,6 +9,40 @@ const Room = (({ roomName, token, handleLogout }) => {
         <p key={participant.sid}>participant.identity</p>
     ));
 
+    useEffect(() => {
+        const participantConnected = participant => {
+            setParticipants(prevParticipants => [...prevParticipants, participant]);
+        };
+        const participantDisconnected = participant => {
+            setParticipants(prevParticipants =>
+                prevParticipants.filter(p => p !== participant)
+            );
+        };
+        Video.connect(token, {
+            name: roomName
+        }).then(room => {
+            setRoom(room);
+            room.on('participantConnected', participantConnected);
+            room.on('participantDisconnected', participantDisconnected);
+            room.participants.forEach(participantConnected);
+        });
+
+        return () => {
+            setRoom(currentRoom => {
+              if (currentRoom && currentRoom.localParticipant.state === 'connected') {
+                currentRoom.localParticipant.tracks.forEach(function(trackPublication) {
+                  trackPublication.track.stop();
+                });
+                currentRoom.disconnect();
+                return null;
+              } else {
+                return currentRoom;
+              }
+            });
+          };
+        }, [roomName, token]);
+
+
     return (
         <div className="room">
             <h2>Room: {roomName}</h2>
