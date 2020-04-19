e57fb98607a29289b638e483672fb4a5fe4b3716
Rohan Sawant <rohansawantct83@gmail.com>
2020-04-19 18:35:44 +0530

    UI fixes

--- static/ui/src/SharedDesk.jsx
+++ static/ui/src/SharedDesk.jsx
@@ -10,8 +10,9 @@ const SharedDesk = ({ roomName, token, handleLogout, handleDisconnect }) => {
     return (
 
         <div>
-            <h1> You are sitting at the desk {roomName}</h1>
-            <button onClick={() => setTalk(!talk)}>Talk}</button>
+            <h1> You are sitting at the desk <em>{roomName}</em> with ...</h1>
+            <p> Say "Hi ..." to talk to them.</p>
+            <button onClick={() => setTalk(!talk)}>Talk</button>
             {talk ? <Room roomName={roomName} token={token} handleLogout={handleLogout} />
                 : null}
         </div>
