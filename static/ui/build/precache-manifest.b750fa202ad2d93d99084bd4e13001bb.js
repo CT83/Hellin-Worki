b062b44a810dfa2fa7499cae8e5dbc5067506920
Rohan Sawant <rohansawantct83@gmail.com>
2020-04-20 19:15:57 +0530

    Added build files

---
+++ static/ui/build/precache-manifest.b750fa202ad2d93d99084bd4e13001bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e227651149e70aaee9068d473f1e3683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce3833bdbe6d5ba6428e",
+    "url": "/static/css/main.29de464f.chunk.css"
+  },
+  {
+    "revision": "5376469d05b6b3b4e028",
+    "url": "/static/js/2.008af1e9.chunk.js"
+  },
+  {
+    "revision": "8c323eee9c0820e0e208cd7dbdc413ce",
+    "url": "/static/js/2.008af1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce3833bdbe6d5ba6428e",
+    "url": "/static/js/main.842bf33c.chunk.js"
+  },
+  {
+    "revision": "d6154063ba32aa26ed47",
+    "url": "/static/js/runtime-main.defe0471.js"
+  }
+]);
