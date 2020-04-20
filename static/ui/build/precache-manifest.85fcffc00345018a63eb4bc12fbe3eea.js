0ae5a38e00df0503372bf3d9a620a547ad6b7b15
Rohan Sawant <rohansawantct83@gmail.com>
2020-04-20 19:29:18 +0530

    Updated build

---
+++ static/ui/build/precache-manifest.85fcffc00345018a63eb4bc12fbe3eea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ee43456655803865fa07a2bcef138a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "405f7e1baba3f788e766",
+    "url": "/static/css/main.29de464f.chunk.css"
+  },
+  {
+    "revision": "5376469d05b6b3b4e028",
+    "url": "/static/js/2.008af1e9.chunk.js"
+  },
+  {
+    "revision": "8c323eee9c0820e0e208cd7dbdc413ce",
+    "url": "/static/js/2.008af1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405f7e1baba3f788e766",
+    "url": "/static/js/main.e23132f1.chunk.js"
+  },
+  {
+    "revision": "d6154063ba32aa26ed47",
+    "url": "/static/js/runtime-main.defe0471.js"
+  }
+]);
